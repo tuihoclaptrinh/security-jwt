9c76bff20b5d71ff01ca529cd55959a849bf822a
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2022-10-15 23:01:14 +0700

    Add services : UserService.

---
+++ src/main/java/com/tuihoclaptrinh/securityjwt/service/Impl/UserServiceImpl.java
@@ -0,0 +1,58 @@
+package com.tuihoclaptrinh.securityjwt.service.Impl;
+
+import com.tuihoclaptrinh.securityjwt.domain.Role;
+import com.tuihoclaptrinh.securityjwt.domain.User;
+import com.tuihoclaptrinh.securityjwt.repo.RoleRepo;
+import com.tuihoclaptrinh.securityjwt.repo.UserRepo;
+import com.tuihoclaptrinh.securityjwt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author : Tuan Anh New Bie Dev
+ * @version : 1.0 | UserServiceImpl
+ * @since : 10:54 PM | 10/15/2022
+ */
+
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
+        User user = userRepo.findByUsername(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        log.info("Fetching user {}", username);
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        log.info("Fetching all users");
+        return userRepo.findAll();
+    }
+}

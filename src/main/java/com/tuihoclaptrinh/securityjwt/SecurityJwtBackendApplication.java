ded999ea7db2e9bba3ccf7e53725c60f06d08082
tuihoclaptrinh <hoangtuananh57tht1@gmail.com>
2022-10-15 22:32:57 +0700

    Innit Project Security JWT.

---
+++ src/main/java/com/tuihoclaptrinh/securityjwt/SecurityJwtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuihoclaptrinh.securityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuihoclaptrinh/securityjwt/SecurityJwtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuihoclaptrinh.securityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
